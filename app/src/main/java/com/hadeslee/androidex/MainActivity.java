cb04b56636e0ebfe3d65244a87d2770801cc0a3e
hadaslee <leesk55@gmail.com>
2015-05-19 15:32:18 +0900

    Toast study

--- app/src/main/java/com/hadeslee/androidex/MainActivity.java
+++ app/src/main/java/com/hadeslee/androidex/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"First Toast", Toast.LENGTH_SHORT).show();
     }
 
     @Override
