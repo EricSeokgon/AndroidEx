23394b699b30a01dd9d9eb8dda6af6cb1c8628e6
hadaslee <leesk55@gmail.com>
2015-05-20 13:21:06 +0900

    CustomView Bitmap add

--- app/src/main/java/com/hadeslee/androidex/CustomView.java
+++ app/src/main/java/com/hadeslee/androidex/CustomView.java
@@ -1,9 +1,13 @@
 package com.hadeslee.androidex;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.View;
 
 /**
@@ -19,7 +23,7 @@ public class CustomView extends View {
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
 
-        canvas.drawColor(Color.GRAY);
+/*      canvas.drawColor(Color.GRAY);
 
         Paint paint = new Paint();
         paint.setColor(Color.RED);
@@ -32,7 +36,17 @@ public class CustomView extends View {
         canvas.drawRect(150, 10, 250, 150, paint);
 
         paint.setColor(0x5000ffff);
-        canvas.drawCircle(200, 200, 100, paint);
+        canvas.drawCircle(200, 200, 100, paint);*/
+
+        canvas.drawColor(Color.LTGRAY);
+        Paint pint = new Paint();
+
+ /*       Resources resources = getResources();
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) resources.getDrawable(R.drawable.wiz);
+        Bitmap bitmap = bitmapDrawable.getBitmap();*/
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.wiz);
+        canvas.drawBitmap(bitmap, 5, 5, null);
 
     }
 }
