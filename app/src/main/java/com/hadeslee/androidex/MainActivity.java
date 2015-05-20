16b86f06d2ec56a37cfacd8a4d923af7b400752a
hadaslee <leesk55@gmail.com>
2015-05-20 10:45:43 +0900

    RelativeLayout parent right left center add

--- app/src/main/java/com/hadeslee/androidex/MainActivity.java
+++ app/src/main/java/com/hadeslee/androidex/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relative_layout);
 
         Toast.makeText(MainActivity.this,"First Toast", Toast.LENGTH_SHORT).show();
     }
