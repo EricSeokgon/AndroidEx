03c7ea6777c83c02ee73d456e3f18128ee87b483
hadesleee <leesk55@gmail.com>
2015-05-22 14:03:10 +0900

    event test

--- app/src/main/java/com/hadeslee/androidex/MainActivity.java
+++ app/src/main/java/com/hadeslee/androidex/MainActivity.java
@@ -1,52 +1,104 @@
 package com.hadeslee.androidex;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
-
-    RelativeLayout rl;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rl = (RelativeLayout) findViewById(R.id.rl01);
-//		rl.setOnClickListener(clickListener);
-        rl.setOnLongClickListener(longClickListener);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        Button bt = (Button) findViewById(R.id.bt);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        EditText et = (EditText) findViewById(R.id.et);
+        SeekBar sb = (SeekBar) findViewById(R.id.sb);
 
-        rl.setOnClickListener(new View.OnClickListener() {
+        tv.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "onClick()", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "tv_onClick()", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(MainActivity.this, "bt_onLongClick()", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        iv.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(MainActivity.this, "iv_onTouch()", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        et.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Toast.makeText(MainActivity.this, "et_onTextChanged() : " + s.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                Toast.makeText(MainActivity.this, "et_beforeTextChanged() : " + s.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Toast.makeText(MainActivity.this, "et_afterTextChanged() : " + s.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "sb_onStopTrackingTouch() : " + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "sb_onStartTrackingTouch() : " + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                Toast.makeText(MainActivity.this, "sb_onProgressChanged() : " + seekBar.getProgress(), Toast.LENGTH_SHORT).show();
+
             }
         });
     }
-
-//	OnClickListener clickListener = new OnClickListener() {
-//
-//		@Override
-//		public void onClick(View v) {
-//			Toast.makeText(MainActivity.this, "onClick()", Toast.LENGTH_SHORT).show();
-//		}
-//	};
-
-    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
-
-        @Override
-        public boolean onLongClick(View v) {
-            Toast.makeText(MainActivity.this, "onLongClick()", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
