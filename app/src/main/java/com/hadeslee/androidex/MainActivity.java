c1e15969c4b964f330ccf5d2f6a635cf68c4756a
hadesleee <leesk55@gmail.com>
2015-05-26 10:29:02 +0900

    MainActivity add

--- app/src/main/java/com/hadeslee/androidex/MainActivity.java
+++ app/src/main/java/com/hadeslee/androidex/MainActivity.java
@@ -1,63 +1,30 @@
 package com.hadeslee.androidex;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    CustomView customView = null;
-    Button newCanvasBtn = null;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        customView = (CustomView) findViewById(R.id.customView);
-        customView.setmContext(MainActivity.this);
+        Button btn = (Button) findViewById(R.id.bt_01);
+        btn.setOnClickListener(new View.OnClickListener() {
 
-        newCanvasBtn = (Button) findViewById(R.id.bt_new_canvas);
-        newCanvasBtn.setOnClickListener(listener);
-
-    }
-
-    View.OnClickListener listener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-
-            switch (v.getId()) {
-                case R.id.bt_new_canvas:
-                    customView.initPaint(CustomView.NEW_CAVAS);
-                    break;
-
-                default:
-                    break;
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
             }
-        }
-    };
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
