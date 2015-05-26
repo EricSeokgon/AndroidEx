d0535c3b89f744984d178e16dcbcc8ff226d95ba
hadesleee <leesk55@gmail.com>
2015-05-26 10:22:05 +0900

    SecondActivity add

---
+++ app/src/main/java/com/hadeslee/androidex/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.hadeslee.androidex;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.*;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+		
+		Button btn = (Button) findViewById(R.id.bt_02);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+	}
+	
+}
