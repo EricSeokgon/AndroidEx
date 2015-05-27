bca8c3f224a9483725738a3ac1ebca4c5bd3dad6
hadesleee <leesk55@gmail.com>
2015-05-27 10:28:12 +0900

    thread add

--- app/src/main/java/com/hadeslee/androidex/MainActivity.java
+++ app/src/main/java/com/hadeslee/androidex/MainActivity.java
@@ -1,92 +1,80 @@
 package com.hadeslee.androidex;
 
-import android.content.*;
-import android.os.*;
-import android.support.v7.app.*;
-import android.view.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-	final static int USER_INFO = 0;
-	
-	TextView tv = null;
-	Button btnGoSecond = null;
-	Button btnGoThird = null;
+    private int mainNum;
+    private int secondNum;
+    private TextView tvMain, tvSecond;
+    private Button btnStart;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		tv = (TextView) findViewById(R.id.tv_main);
-		btnGoSecond = (Button) findViewById(R.id.bt_go_second);
-		btnGoSecond.setOnClickListener(listener);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		btnGoThird = (Button) findViewById(R.id.bt_go_third);
-		btnGoThird.setOnClickListener(listener);
-	}
+        tvMain = (TextView) findViewById(R.id.tv_main_thead);
+        tvSecond = (TextView) findViewById(R.id.tv_second_thread);
+        btnStart = (Button) findViewById(R.id.bt_start);
 
-	OnClickListener listener = new OnClickListener() {
+        btnStart.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNum();
+            }
+        });
+    }
 
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			
-			Intent intent = null;
-			
-			switch (v.getId()) {
-			case R.id.bt_go_second:
+    private void startNum() {
+        mainNum++;
 
-				intent = new Intent(MainActivity.this,
-						SecondActivity.class);
-				intent.putExtra("u_id", "abc");
-				intent.putExtra("u_pw", 1234);
-				startActivity(intent);
-				break;
+        NewThread newThread = new NewThread();
+        newThread.setDaemon(true);
+        newThread.start();
 
-			case R.id.bt_go_third:
+        tvMain.setText("mainNum : " + mainNum);
+        tvSecond.setText("secondNum : " + secondNum);
 
-				intent = new Intent(MainActivity.this,
-						ThirdActivity.class);
-				intent.putExtra("u_id", "abcd");
-				intent.putExtra("u_pw", 1234);
-				startActivityForResult(intent, MainActivity.USER_INFO);
-				break;
-			}
-			
-		}
-	};
+    }
 
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		switch (requestCode) {
-		case MainActivity.USER_INFO:
-			if (resultCode == ThirdActivity.LOG_IN_OK) {
-				tv.setText("LogIn Success!, " + intent.getStringExtra("user_info"));
-			} else if (resultCode == ThirdActivity.LOG_IN_NG){
-				tv.setText("LogIn Fail!, " + intent.getStringExtra("user_info"));
-			}
-			break;
-		}
-	};
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-	//	getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    class NewThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                secondNum++;
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //	getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
