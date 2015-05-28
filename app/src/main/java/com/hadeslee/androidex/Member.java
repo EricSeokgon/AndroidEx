ca0b819ae921606b333ba3b345f5ec5361573278
hadesleee <leesk55@gmail.com>
2015-05-28 11:24:18 +0900

    Member class add

---
+++ app/src/main/java/com/hadeslee/androidex/Member.java
@@ -0,0 +1,12 @@
+package com.hadeslee.androidex;
+
+/**
+ * Created by sklee on 2015-05-28.
+ */
+public class Member {
+    private String name;
+    private int age;
+    private String[] hobby;
+    private String v_id;
+    private String v_pw;
+}
